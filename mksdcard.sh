0d44b59f92e868423d0898ef26940c35df0ac38f
Alexander Kappner <alexander@kappner.info>
2025-10-12 20:54:19 -0700

    initial commit

---
+++ mksdcard.sh
@@ -0,0 +1,9 @@
+touch /tmp/trash.bin
+truncate --size $((2**32)) /tmp/trash.bin
+parted -s /tmp/trash.bin mklabel msdos mkpart primary fat32 1MiB 3500MiB
+mkfs.vfat -n NAVDB -v --offset 2048 /tmp/trash.bin
+
+touch /tmp/trash2.bin
+truncate --size $((2**32)) /tmp/trash2.bin
+parted -s /tmp/trash2.bin mklabel msdos mkpart primary fat32 1MiB 3500MiB
+mkfs.vfat -n SUPP -v --offset 2048 /tmp/trash2.bin
